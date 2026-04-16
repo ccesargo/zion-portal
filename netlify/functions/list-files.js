f0fd6bb882527833655ddb66033928a2b478f380
ccesargo <ccesargo@gmail.com>
2026-04-16 13:16:27 -0400

    Update list-files.js

--- netlify/functions/list-files.js
+++ netlify/functions/list-files.js
@@ -1,27 +1,40 @@
-const { GoogleAuth } = require("google-auth-library");
-const { google } = require("googleapis");
+const {google} = require("googleapis");
 
 async function getAuthClient() {
-  const credentials = JSON.parse(process.env.GDRIVE_SERVICE_ACCOUNT);
-  const auth = new GoogleAuth({ credentials, scopes:["https://www.googleapis.com/auth/drive.readonly"] });
-  return auth.getClient();
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GDRIVE_CLIENT_ID,
+    process.env.GDRIVE_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+  oauth2Client.setCredentials({refresh_token: process.env.GDRIVE_REFRESH_TOKEN});
+  return oauth2Client;
 }
 
-exports.handler = async (event) => {
-  const headers = { "Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"Content-Type","Content-Type":"application/json" };
-  if (event.httpMethod === "OPTIONS") return { statusCode:200, headers, body:"" };
+exports.handler = async function(event) {
+  const h = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Content-Type": "application/json"
+  };
+
+  if (event.httpMethod === "OPTIONS") return {statusCode: 200, headers: h, body: ""};
+
   try {
-    const folderId = event.queryStringParameters?.folderId;
-    if (!folderId) throw new Error("folderId obrigatório");
+    const folderId = event.queryStringParameters && event.queryStringParameters.folderId;
+    if (!folderId) throw new Error("folderId obrigatorio");
+
     const authClient = await getAuthClient();
-    const drive = google.drive({ version:"v3", auth:authClient });
+    const drive = google.drive({version: "v3", auth: authClient});
+
     const res = await drive.files.list({
-      q: `'${folderId}' in parents and trashed=false`,
+      q: "'" + folderId + "' in parents and trashed=false",
       fields: "files(id,name,mimeType,size,createdTime,webViewLink)",
-      orderBy: "createdTime desc",
+      orderBy: "createdTime desc"
     });
-    return { statusCode:200, headers, body: JSON.stringify({ files:res.data.files }) };
-  } catch(err) {
-    return { statusCode:500, headers, body: JSON.stringify({error:err.message}) };
+
+    return {statusCode: 200, headers: h, body: JSON.stringify({files: res.data.files})};
+  } catch(e) {
+    console.error("list-files error:", e.message);
+    return {statusCode: 500, headers: h, body: JSON.stringify({error: e.message})};
   }
 };
